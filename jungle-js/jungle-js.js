c16baa71f9ad1b52e2bb64212faa7f70a9f30be0
jungle <wj00@qq.com>
2014-09-10 23:06:54 +0800

    main
    do it!

---
+++ jungle-js/jungle-js.js
@@ -0,0 +1,11 @@
+/*!
+ * Jungle JavaScript framework v0.1
+ * http://www.biorecursive.com/
+ *
+ * Copyright 2014 wangjun
+ * Released under the BSD license
+ *
+ * Date: 2014-09-10T23:05Z
+ */
+
+window.jungle=jungle||{};
