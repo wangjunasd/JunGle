a163439e4fb35c033aaf751011f2dc98502e5eaf
jungle <wj00@qq.com>
2014-09-10 23:08:58 +0800

    changed detail of copyright.

--- jungle-php/index.php
+++ jungle-php/index.php
@@ -1,6 +1,12 @@
 <?php
-/**
- * @author jungle
+/*!
+ * Jungle PHP framework v0.1
+ * http://www.biorecursive.com/
+ *
+ * Copyright 2014 wangjun
+ * Released under the BSD license
+ *
+ * Date: 2014-09-10T23:05Z
  */
 //路由
 $router=new router();
